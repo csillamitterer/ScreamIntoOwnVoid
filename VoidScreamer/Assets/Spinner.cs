bd4ce011889ca51777dc2efcd1a73f7af7779717
Giles Excell <9713395@student.swin.edu.au>
2015-11-28 19:16:59 +1100

    power to food, spinning rooms, layout changes

---
+++ VoidScreamer/Assets/Spinner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+	public float speedFactor = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (transform.forward * Time.deltaTime * speedFactor);
+	}
+}
