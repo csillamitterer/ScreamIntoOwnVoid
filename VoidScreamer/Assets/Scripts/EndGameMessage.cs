9398610dd7fa319201d99dd068de60997b90044b
Giles Excell <9713395@student.swin.edu.au>
2015-11-29 16:25:23 +1100

    Endgame message fix

--- VoidScreamer/Assets/Scripts/EndGameMessage.cs
+++ VoidScreamer/Assets/Scripts/EndGameMessage.cs
@@ -6,13 +6,13 @@ public class EndGameMessage : MonoBehaviour {
 
 	void EndGame(string type) {
 		if (type == "Fuel") {
-			gameObject.GetComponent<Text> ().text = "YOU RAN OUT OF FUEL\n\n\nYOU LOSE";
+			gameObject.GetComponent<Text> ().text = "YOU RAN OUT OF FUEL\n\n\n\nYOU LOSE";
 		} else if (type == "Population") {
-			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\nYOU LOSE";
+			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\n\nYOU LOSE";
 		} else if (type == "Food") {
-			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\nYOU LOSE";
+			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\n\nYOU LOSE";
 		} else if (type == "Water") {
-			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\nYOU LOSE";
+			gameObject.GetComponent<Text> ().text = "EVERYONE DIED\n\n\n\nYOU LOSE";
 		} else if (type == "Win") {
 			gameObject.GetComponent<Text> ().text = "WELL DONE\n\n\n\nYOU WIN";
 		}
